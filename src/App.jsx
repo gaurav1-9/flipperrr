4c0ba4a21ca02fe49f37915ef7043e90497e7050
Gaurav <gauravkrdas19@gmail.com>
2025-07-09 00:34:47 +0530

    title & footer UI done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Title from './components/Title'
+import Footer from './components/footer'
 
 const App = () => {
   return (
     <div>
       <Title />
+      <Footer />
     </div>
   )
 }
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Title = () => {
   return (
-    <div className='text-butterscotch w-fit mt-6 pl-10.5 lg:pl-30'>
-        <p className='font-rampartOne text-6xl'>flipperrr</p>
+    <div className='text-butterscotch mt-6 px-10.5 lg:pl-30 flex w-full md:w-fit flex-col items-center md:items-start'>
+        <p className='font-rampartOne text-6xl'>flipperrr</p> 
         <p className='font-montserratAlt uppercase leading-12 font-semibold text-lg text-center tracking-wide'>a memory card game</p>
     </div>
   )
