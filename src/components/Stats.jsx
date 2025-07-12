78e19424e21813e060bf91b2c89dfc0933c3aba7
Gaurav <gauravkrdas19@gmail.com>
2025-07-12 21:53:48 +0530

    divider added

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Timer from './Timer'
 
-const Stats = ({ minutes, seconds , formattedMoves }) => {
-  
+const Stats = ({ minutes, seconds, formattedMoves }) => {
+
   return (
     <div className="w-full h-fit p-3 text-butterscotch flex flex-col justify-center items-start font-montserratAlt font-semibold">
-      <div className="hidden lg:flex">
-        <Timer minutes={minutes} seconds={seconds} formattedMoves={formattedMoves}/>
+      <div className="hidden lg:flex flex-col">
+        <Timer minutes={minutes} seconds={seconds} formattedMoves={formattedMoves} />
+        <div className="w-full h-1 bg-butterscotch/30 mt-5"></div>
       </div>
     </div>
   )
