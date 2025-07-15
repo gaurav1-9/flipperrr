eff04123dca6ac75b20c5a2b7e2e1787e0c7c9d3
Gaurav <gauravkrdas19@gmail.com>
2025-07-16 00:43:22 +0530

    hitn btn UI fixing

--- src/components/PlayArea.jsx
+++ src/components/PlayArea.jsx
@@ -10,16 +10,16 @@ const PlayArea = ({ minutes, seconds, formattedMoves, cardSet, generatingGame, s
     <div className='relative border-butterscotch w-full h-fit md:aspect-square p-4 border-4 rounded-lg text-butterscotch font-montserratAlt flex flex-col justify-start items-center font-semibold'>
       <p className='text-2xl text-start mb-2'>play area</p>
       <div
-        className={`bg-eerieBlack absolute top-13 md:top-4 md:right-4 flex-col w-20 md:w-fit md:flex-row justify-center items-center px-4 py-1 rounded-md gap-1 lg:gap-1.5 hover:bg-eerieBlack/60 cursor-pointer ${(!showHint.visibility && cardSet.length)
+        className={`bg-eerieBlack absolute top-24 md:top-4 right-0.5 md:right-4 flex-row w-fit justify-center items-center px-4 py-2 md:py-1 rounded-md gap-1 md:gap-1.5 hover:bg-eerieBlack/60 cursor-pointer ${(!showHint.visibility && cardSet.length)
           ? 'flex'
           : 'hidden'
-          } min-[375px]:scale-70 min-[375px]:right-0 md:scale-100`}
+          } scale-75 md:scale-100`}
         onClick={() => { if (generatingGame === false && cardSet) hint() }}
       >
         <HiOutlineLightBulb className='text-2xl' />
         <p className='text-sm'>{showHint.content}</p>
         {
-          showHint.subContent && <p className='text-xs leading-1 md:leading-normal pb-3 md:pb-0'>({showHint.subContent})</p>
+          showHint.subContent && <p className='text-xs leading-normal md:leading-normal pb-0 md:pb-0'>({showHint.subContent})</p>
         }
       </div>
       <div className="flex justify-start w-full lg:hidden mt-2">
